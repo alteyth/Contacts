41c5c923f0d4c585844178c1dd3f604a45bd33f0
Simone Seria <simoneseria@live.com>
2025-08-07 22:28:33 +0200

    Add Persona class to represent individual contact information

---
+++ Rubrica/Persona.java
@@ -0,0 +1,17 @@
+package Rubrica;
+
+public class Persona {
+    String nome;
+    String cognome;
+    String indirizzo;
+    String telefono;
+    int eta;
+
+    public Persona(String nome, String cognome, String indirizzo, String telefono, int eta) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.indirizzo = indirizzo;
+        this.telefono = telefono;
+        this.eta = eta;
+    }
+}
